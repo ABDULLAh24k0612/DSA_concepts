50e8493d27440cf4cafdd788ce41be9c497a3682
ABDULLAh <fahadkamran2103@gmail.com>
2025-09-20 17:15:53 -0700

    Update combSort.cpp

--- elementarySorting/combSort.cpp
+++ elementarySorting/combSort.cpp
@@ -1,1 +1,18 @@
 
+void combSort(int arr[], int n) {
+    int gap = n;
+    bool swapped = true;
+
+    while (gap > 1 || swapped) {
+        gap = (gap * 10) / 13; // shrink gap
+        if (gap < 1) gap = 1;
+
+        swapped = false;
+        for (int i = 0; i + gap < n; i++) {
+            if (arr[i] > arr[i + gap]) {
+                swap(arr[i], arr[i + gap]);
+                swapped = true;
+            }
+        }
+    }
+}
