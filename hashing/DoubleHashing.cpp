d3c44f938e785c9bda127f30afcbbfb3db7b4130
ABDULLAh <fahadkamran2103@gmail.com>
2025-12-13 02:57:28 -1200

    Update DoubleHashing.cpp

--- hashing/DoubleHashing.cpp
+++ hashing/DoubleHashing.cpp
@@ -1,63 +1,71 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class DoubleHash {
+class doublehash{
+    public:
+
     int size;
-    int* table;
-    int EMPTY = -1;
+    int*table;
 
-public:
-    DoubleHash(int s) {
-        size = s;
-        table = new int[size];
-        for (int i = 0; i < size; i++)
-            table[i] = NULL;
-    }
-
-    int h1(int key) {
-        return key % size;
-    }
-
-    int h2(int key) {
-        return 7 - (key % 7);  
-    }
-
-    void insertKey(int key) {
-        int u = h1(key);
-        int v = h2(key);
-
-        int i = 0;
-        while (table[(u + i * v) % size] != EMPTY){
-            i++;
+    doublehash(int s){
+        size=s;
+        table=new int[size];
+        for(int i=0;i<size;i++){
+            table[i]=NULL;
         }
-
-        table[(u + i * v) % size] = key;
     }
 
-    bool searchKey(int key) {
-        int u = h1(key);
-        int v = h2(key);
+    int hash1(int key){
+        return key%size;
+    }
 
-        int i = 0;
-        while (table[(u + i * v) % size] != EMPTY) {
-            if (table[(u + i * v) % size] == key)
+    int hash2(int key){
+        return 7-(key%7);
+    }
+
+    void insert(int key){
+       int u=hash1(key);
+       int v=hash2(key);
+        
+        for(int i=0;i<size;i++){
+            int newindex=(u+i*v)%size;
+            if(table[newindex]==NULL){
+                table[newindex]=key;
+                break;
+            }
+        }
+    }
+    
+    void deletekey(int key){
+        int u=hash1(key);
+       int v=hash2(key);
+
+        for(int i=0;i<size;i++){
+            int newindex=(u+i*v)%size;
+            if(table[newindex]==key){
+                table[newindex]=NULL;
+                return;
+            }
+        }
+        cout<< key<< "not found in the table"<<endl;
+    }
+
+    bool searchkey(int key){
+       int u=hash1(key);
+       int v=hash2(key);
+
+        for(int i=0;i<size;i++){
+           int newindex=(u+i*v)%size;
+            if(table[newindex]==key){
                 return true;
-            i++;
+            }
         }
+        cout<< key<< "not found in the table"<<endl;
         return false;
     }
-
-    void display() {
-        for (int i = 0; i < size; i++)
-            cout << i << " : " << table[i] << endl;
+    void display(){
+        for(int i=0;i<size;i++){
+            cout<< i << ":"<< table[i]<<endl;
+        }
     }
 };
-
-int main() {
-    DoubleHash h(10);
-    h.insertKey(12);
-    h.insertKey(22);
-    h.insertKey(32);
-
-    h.display();
-}
